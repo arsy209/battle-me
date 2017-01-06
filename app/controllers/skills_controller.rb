2a661c5d8458b76ed0249b5d720d68c4ef5cad86
Arshdeep Singh <singh_arsy@yahoo.com>
2017-01-06 09:10:05 -0500

    create new,edit,destory, update routes and files for skills

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -5,4 +5,51 @@ class SkillsController < ApplicationController
     navigation_add("Skill Index", "#")
 	end
 
+	def show
+		@skill = Skill.find(params[:id])
+		navigation_add("Skill Index", skills_path)
+		navigation_add("Skill Show", "#")
+	end
+
+	def create
+		@skill = Skill.new(skill_params)
+		if @skill.valid?
+			@skill.save
+			redirect_to @skill
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@skill = Skill.find(params[:id])
+		navigation_add("Skill Index", skills_path)
+		navigation_add("Skill Show", @skill)
+		navigation_add("Edit Skill", "#")
+	end
+
+	def update
+		@skill = Skill.find(params[:id])
+		@skill.update(skill_params)
+		if @skill.valid?
+			@skill.save
+			redirect_to @skill
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@skill = Skill.find(params[:id])
+		name = @skill.name
+		@skill.destroy
+		flash[:notice] = "The skill #{name} was removed."
+		redirect_to skills_path
+	end
+
+	private
+	
+	def skill_params
+	params.require(:skill).permit(:name, :power, :max_pp, :element_type)
+	end
 end
