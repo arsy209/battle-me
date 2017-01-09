dc2cad9e16ba292e88e367ced9a38106f2bc941b
Arshdeep Singh <singh_arsy@yahoo.com>
2017-01-08 20:46:40 -0500

    trying to fix bug

--- lib/pokemon_battle_ai_engine.rb
+++ lib/pokemon_battle_ai_engine.rb
@@ -1,55 +1,56 @@
 class PokemonBattleAiEngine
-  def initialize(pokemon_battle:)
-    @pokemon_battle = pokemon_battle
-    @player = @pokemon_battle.pokemon1
-    @ai = @pokemon_battle.pokemon2
-  end
+	def initialize(pokemon_battle:)
+		@pokemon_battle = pokemon_battle
+		@player = @pokemon_battle.pokemon1
+		@ai = @pokemon_battle.pokemon2
+	end
 
-  def start_ai_battle?
-    results = []
-    results << validate_state?
-    results << validate_battle_type?
-    results << validate_health_point?
-    results.all?
-  end
+	def start_ai_battle?
+		result = []
+		result << validate_state?
+		result << validate_battle_type?
+		result << validate_health_point?
 
-  def begin_ai_battle
-    if start_ai_battle?
-      start_ai_battle_engine
-    else
-      @pokemon_battle.errors.add(:state,"No battle can be take at this time")
-    end
-  end
+		result.all?
+	end
 
-  private
+	def begin_ai_battle
+		if start_ai_battle?
+			start_ai_battle_engine
+		else
+			@pokemon_battle.errors.add(:state, "Cannot battle at this point.")
+		end
+	end
 
-  def start_ai_battle_engine
-    pickable_skills = @ai.pokemon_skills.where("current_pp >?",0)
-    total_skills = pickable_skills.count
-    pick_number = rand(0..total_skills-1)
-    skill = pickable_skills[pick_number] unless pick_number == nil
+	private
+	def start_ai_battle_engine
+		pickable_skills = @ai.pokemon_skills.where("current_pp > ?", 0)
+		total_skills = pickable_skills.count
 
-    if skill.nil?
-      pokemon_battle_engine = PokemonBattleEngine.new(
-      pokemon_battle: @pokemon_battle,
-      attacker_id: @ai.id)
-    else
-      pokemon_battle_engine = PokemonBattleEngine.new(
-      pokemon_battle: @pokemon_battle,
-      attacker_id: @ai.id,
-      skill_id: skill.id)
-    end
+		pick_number = rand(0..total_skills-1)
+		skill = pickable_skills[pick_number] unless pick_number == nil
 
-    if pokemon_battle_engine.list_attack_validations?
-      pokemon_battle_engine.attack
-      pokemon_battle_engine.save_attack
-    elsif pokemon_battle_engine.list_surrender_validations?
-      pokemon_battle_engine.surrender
-      pokemon_battle_engine.save_surrender
-    end
-  end
+		if skill.nil?
+			pokemon_battle_engine = PokemonBattleEngine.new(
+				pokemon_battle: @pokemon_battle,
+				attacker_id: @ai.id)
+		else
+			pokemon_battle_engine = PokemonBattleEngine.new(
+				pokemon_battle: @pokemon_battle,
+				attacker_id: @ai.id,
+				skill_id: skill.id)
+		end
 
-  def validate_state?
+		if pokemon_battle_engine.list_attack_validations?
+			pokemon_battle_engine.attack
+			pokemon_battle_engine.save_attack
+		elsif pokemon_battle_engine.list_surrender_validations?
+			pokemon_battle_engine.surrender
+			pokemon_battle_engine.save_surrender
+		end
+	end
+
+	def validate_state?
 		if @pokemon_battle.state == "Ongoing"
 			flag = true
 		else
