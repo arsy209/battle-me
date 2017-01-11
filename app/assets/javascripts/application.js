7055d82cd84feb3ec3b93e3e04f42f8cfcd73e12
Arshdeep Singh <singh_arsy@yahoo.com>
2017-01-11 17:57:13 -0500

    change navbar, and fix spacing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,24 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function() {
+     $('.nav_bar').click(function(){
+     $('.navigation').toggleClass('visible');
+   $('body').toggleClass('opacity');
+ });
+
+     $('.plus a img').on('click', function () {
+         console.log('plus clicked');
+         $('.first-tab-at services-top services-top-toggle').toggle();
+         $('.plus').hide();
+         $('.minus').show();
+     });
+     $('.minus a img').on('click', function () {
+         console.log('minus clicked');
+         $('.first-tab-at services-top services-top-toggle').toggle();
+         $('.minus').hide();
+         $('.plus').show();
+     });
+
+});
