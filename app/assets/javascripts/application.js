a5c85584842564d86b271bee02a8c817ce0d1bf4
Arshdeep Singh <singh_arsy@yahoo.com>
2017-01-11 18:27:40 -0500

    fix spacing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,22 +15,20 @@
 //= require_tree .
 
 $(document).ready(function() {
-     $('.nav_bar').click(function(){
-     $('.navigation').toggleClass('visible');
+   $('.nav_bar').click(function(){
+   $('.navigation').toggleClass('visible');
    $('body').toggleClass('opacity');
  });
-
-     $('.plus a img').on('click', function () {
-         console.log('plus clicked');
-         $('.first-tab-at services-top services-top-toggle').toggle();
-         $('.plus').hide();
-         $('.minus').show();
-     });
-     $('.minus a img').on('click', function () {
-         console.log('minus clicked');
-         $('.first-tab-at services-top services-top-toggle').toggle();
-         $('.minus').hide();
-         $('.plus').show();
-     });
-
+ $('.plus a img').on('click', function () {
+   console.log('plus clicked');
+   $('.first-tab-at services-top services-top-toggle').toggle();
+   $('.plus').hide();
+   $('.minus').show();
+ });
+ $('.minus a img').on('click', function () {
+   console.log('minus clicked');
+   $('.first-tab-at services-top services-top-toggle').toggle();
+   $('.minus').hide();
+   $('.plus').show();
+ });
 });
