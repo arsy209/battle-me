1f3e89c1de55186d4ce8b448b3e047a238bae56d
Arshdeep Singh <singh_arsy@yahoo.com>
2017-03-07 11:10:43 -0500

    Update skills_controller.rb

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -45,6 +45,6 @@ class SkillsController < ApplicationController
 	private
 
 	def skill_params
-	params.require(:skill).permit(:name, :power, :max_pp, :element_type)
+		params.require(:skill).permit(:name, :power, :max_pp, :element_type)
 	end
 end
